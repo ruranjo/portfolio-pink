2a3860b8b9fd6f1fef28e998b158a9976e1dea42
ruranjo <rubens.rangel.job@gmail.com>
2024-04-18 08:17:43 -0500

    added iconLink

---
+++ src/utils/data.ts
@@ -0,0 +1,33 @@
+interface IconInfo {
+    name: string;
+    address: string;
+    icon: string;
+}
+
+const icons: IconInfo[] = [
+    {
+        name: "IconEmail",
+        address: "correo@eemplo.com",
+        icon: "ruta/al/icono/email.png"
+    },
+    {
+        name: "IconLinkedin",
+        address: "https://www.linkedin.com/in/tu-perfil",
+        icon: "ruta/al/icono/linkedin.png"
+    },
+    {
+        name: "IconX",
+        address: "https://www.example.com",
+        icon: "ruta/al/icono/x.png"
+    },
+    {
+        name: "IconGithub",
+        address: "https://github.com/tu-usuario",
+        icon: "ruta/al/icono/github.png"
+    },
+    {
+        name: "IconYoutube",
+        address: "https://www.youtube.com/c/tu-canal",
+        icon: "ruta/al/icono/youtube.png"
+    }
+];
