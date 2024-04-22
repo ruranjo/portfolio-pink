4cceacfc50179a57dd8c329344d13335c54a8822
ruranjo <rubens.rangel.job@gmail.com>
2024-04-22 08:17:18 -0500

    fix 404

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import { About, Blog, Home, Layout, NotFound, Portfolio, Publications } from './pages';
 
-const App:React.FC<{}> = () => {
+const App: React.FC<{}> = () => {
   return (
     <Router>
       <Routes>
-       {/* Página de inicio sin Layout */}
-       <Route path="/portfolio-pink/" element={<Home />} />
+        {/* Página de inicio sin Layout */}
+        <Route path="/portfolio-pink/" element={<Home />} />
        
         {/* Resto de las páginas con Layout */}
-        <Route element={<Layout />}>
-          <Route path="/portfolio-pink/about" element={<About />} />
-          <Route path="/portfolio-pink/blog" element={<Blog />} />
-          <Route path="/portfolio-pink/blog/:nameblog" element={<Blog />} />
-          <Route path="/portfolio-pink/portfolio" element={<Portfolio />} />
-          <Route path="/portfolio-pink/publications" element={<Publications />} />
+        <Route path="/portfolio-pink/*" element={<Layout />}>
+          <Route path="about" element={<About />} />
+          <Route path="blog" element={<Blog />} />
+          <Route path="blog/:nameblog" element={<Blog />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="publications" element={<Publications />} />
           <Route path="*" element={<NotFound />} />
         </Route>
-        
-        {/* Ruta comodín para páginas no encontradas */}
-        
       </Routes>
     </Router>
   );
