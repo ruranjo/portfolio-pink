5aecbd7121ac2df49b3924c44aabe396bf7910f1
ruranjo <rubens.rangel.job@gmail.com>
2024-04-22 08:30:50 -0500

    fix url blog:id

--- src/components/ItemBlog/ItemBlog.tsx
+++ src/components/ItemBlog/ItemBlog.tsx
@@ -10,7 +10,7 @@ export interface Props {
 
 const ItemBlog:React.FC<Props> = ({ title, date, description, id }) => {
   return (
-    <Link to={`/blog/${id}`} className="blog-item-link">
+    <Link to={`/portfolio-pink/blog/${id}`} className="blog-item-link">
       <div className="blog-item w-full bg-lightPink p-8 shadow-lg transition duration-300 ease-in-out transform hover:scale-[1.01]  hover:shadow-lg">
         <h2 className="text-3xl font-medium text-primary">{title}</h2>
         <p className="blog-item-date italic">{date}</p>
